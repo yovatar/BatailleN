58ede03e03de4469fb63f42ccd5bcc8df3f704a7
Yoann.BONZON <yoann.bonzon@bluewin.ch>
2020-03-06 14:59:35 +0100

    Creation variable accueil

--- BatailleNavaleYB/main.c
+++ BatailleNavaleYB/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+int accueil(int choix) {
+    printf("-----Bataille Navale-----\n");
+    printf("\n-Bien venu sur la Bataille Navale Choisisez une option-\n");
+    printf("\n1-Jouer");
+    printf("\n2-pseudo");
+    printf("\n3-aide");
+    printf("\n4-quitter");
+    scanf("%d",&choix);
+    return choix;
+}
+//int witcher(){
+  //  return 0
+//}
 
 int main() {
-    printf("Hello, World!\n");
+    int accueilChoix=5;
+    accueilChoix=accueil(accueilChoix);
     return 0;
 }
