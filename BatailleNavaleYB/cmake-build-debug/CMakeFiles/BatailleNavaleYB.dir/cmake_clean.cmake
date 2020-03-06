c0b28f8baf25f8a0562d831c8483adb046d2c811
Yoann.BONZON <yoann.bonzon@bluewin.ch>
2020-03-06 14:17:06 +0100

    Commancement du projet

---
+++ BatailleNavaleYB/cmake-build-debug/CMakeFiles/BatailleNavaleYB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BatailleNavaleYB.exe"
+  "BatailleNavaleYB.exe.manifest"
+  "BatailleNavaleYB.pdb"
+  "CMakeFiles/BatailleNavaleYB.dir/main.c.obj"
+  "libBatailleNavaleYB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BatailleNavaleYB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
