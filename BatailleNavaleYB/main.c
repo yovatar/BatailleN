3e8e75b726d0aeb59cc2cc4077c840705de7a9dd
yovatar <yovatar@users.noreply.github.com>
2020-04-08 20:59:51 +0200

    Version final de la bataille naval 2.0

--- BatailleNavaleYB/main.c
+++ BatailleNavaleYB/main.c
@@ -12,6 +12,13 @@
  * @version 0.1.1
  * @details les fonction sont mieux appeler et menu aide dispo et quitter fonctionne
  */
+ /**
+  * @version 2.0
+  * @details version totalement fini de la bataille naval
+  */
+  /**
+   * @remark la version final comprends un menu d'aide une bataille naval totalemnt fonctionnelle et un menu quitter
+   */
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
@@ -26,6 +33,8 @@
 char nom[15];
 int grillechoisie = 0;
 int score = 0;
+int nbBateauxCoules=0;
+//le tableau qui me sers de grille de jeux
 int grille[4][dimentonsGrille][dimentonsGrille] = {{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                                     {0, 0, 0, 4, 0, 0, 0, 0, 0, 0},
@@ -93,22 +102,28 @@ void GrilleJeux() {
         printf("%2d", y + 1);
         printf("|");
         for (x = 0; x <= dimentonsGrille - 1; x++) {
-
+            //se switch ecrit le caractére pour faire en sorte d'avoir une croix ou autres
             switch (grille[grillechoisie][y][x]) {
 
                 case 1: {
                     printf("   ");
                     break;
                 }
-                case 2: {
+                case 13: {
                     printf(" X ");
                     break;
                 }
-                case 3: {
+                case 5:
+                case 6:
+                case 7:
+                case 8:{
                     printf(" T ");
                     break;
                 }
-                case 4: {
+                case 9:
+                case 10:
+                case 11:
+                case 12: {
                     printf(" C ");
                     break;
                 }
@@ -125,7 +140,61 @@ void GrilleJeux() {
 
 
 }
+//fonction qui gere l'état de la grille
+int comptenomdresdunetat(int etat){
+    int i,j;
+    int nbEtat=0;
+    for(i = 0; i<=dimentonsGrille-1;i++){
+        for(j = 0; j<=dimentonsGrille-1;j++){
+            if(grille[grillechoisie][i][j] == etat){
+                nbEtat+=1;
+            }
+        }
+    }
 
+
+
+    return nbEtat;
+}
+//fonction qui gére l'état des case de la grille
+void changeEtatToucheACoule(int etat){
+    int i,j;
+    int nbEtat=0;
+    for(i = 0; i<=dimentonsGrille-1;i++){
+        for(j = 0; j<=dimentonsGrille-1;j++){
+            if(grille[grillechoisie][i][j] == etat){
+                grille[grillechoisie][i][j]+=4;
+
+            }
+        }
+    }
+
+}
+
+//fonction qui gére le fait d'avoir un nombre de bataux coulé
+void verifcoules(){
+    int nbtouchebateau[4];
+    int i;
+
+    for(i=1;i<=4;i++){
+        nbtouchebateau[i-1] = comptenomdresdunetat(i+4);
+
+    }
+
+    for(i=1;i<=4;i++){
+        if(nbtouchebateau[i-1] == i){
+            changeEtatToucheACoule(i+4);
+            nbBateauxCoules+=1;
+            score+=10;
+        }
+    }
+
+
+
+
+}
+
+//la fonction de tire elle demande les axe etc pour que le joueur puisse designer une case a attaquer
 void tir() {
     char posY, posX;
     int j;
@@ -147,15 +216,18 @@ void tir() {
         posY--;
     } while (posY < 0 || posY > dimentonsGrille);
 
-
+    //switch qui s'occupe de gérer les tire
     switch (grille[grillechoisie][posY][posX]) {
         case 0: {
-            grille[grillechoisie][posY][posX] = 2;
+            grille[grillechoisie][posY][posX] = 13;
             score -= 1;
             break;
         }
-        case 1: {
-            grille[grillechoisie][posY][posX] = 3;
+        case 1:
+        case 2:
+        case 3:
+        case 4: {
+            grille[grillechoisie][posY][posX] += 4;
             score += 2;
             break;
         }
@@ -166,8 +238,10 @@ void tir() {
         }
     }
 
-}
+    verifcoules();
 
+}
+ // fonction du choix de la grille elle utilise le temps réelle pour faire en sorte que la grille sois random
 int choixgrille() {
     long ms; // Milliseconds
     time_t s;  // Seconds
@@ -185,21 +259,29 @@ int choixgrille() {
 
 }
 
+//fonction qui gére l'affichage de tout le jeux
 void jeu() {
-    int i;
+    int nbtir = 0;
     choixgrille();
-    for (i = 0; i <= 20; i++) {
-        GrilleJeux();
-        tir();
 
+    while(nbtir<30&&nbBateauxCoules<4) {
+        GrilleJeux();
+        printf("%d", grillechoisie);
+        printf("nbcoule=%d",nbBateauxCoules);
+        tir();
 
     }
 
+
     GrilleJeux();
+    if (nbBateauxCoules==4){
+        printf("\nvous avez Gangné\n");
+    }
+
     system("pause");
 }
 
-
+// fonction qui affiche le menu d'aide du jeux
 int aide() {
     printf("\n-----BIEN VENU SUR L'AIDE DE LA BATAILLE NAVALE-----\n");
     printf("\nO = Bateaux placé");
